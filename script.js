ff16ac86c89c9b10cbc21a58ffbf4fccbd66ce67
TimothyP791 <47483875+TimothyP791@users.noreply.github.com>
2023-07-11 12:46:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var groceries = ['milk', 'eggs', 'chesse'];
+
+
+function listGroceries() {
+    for (var i = 0; i < groceries.length; i++){
+        console.log(groceries[i]);
+    }
+}
+
+listGroceries();
+
+document.getElementById('box').addEventListener('click', function(){
+    alert('I am hovering');
+});
